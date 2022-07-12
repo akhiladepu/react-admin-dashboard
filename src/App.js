6033315f144917c0fe0aa613536107e88d4369ac
akhiladepu <akhil9394021176@gmail.com>
2022-07-12 15:54:49 +0530

    Initial setup, tailwind and craco configuration complete

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className='underline text-3xl'>
+      React Admin Dashboard
+    </div>
+  );
+}
+
+export default App;
