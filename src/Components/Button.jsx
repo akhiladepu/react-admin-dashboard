a3abc169e8bfc4680b5a2aee9a06aa0e9372372c
akhiladepu <akhil9394021176@gmail.com>
2022-07-12 20:03:22 +0530

    Home page ( Ecommerce )

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export const Button = () => {
+export const Button = ({ color, bgColor, size, text, borderRadius }) => {
   return (
-    <div>Button</div>
+    <button type="button" style={{ backgroundColor: bgColor, borderRadius, color }} className={`text-${size} p-3 hover:drop-shadow-xl`} >
+      {text}
+    </button>
   )
 }
