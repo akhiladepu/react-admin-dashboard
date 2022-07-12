a13fb1fdaafe46d8c327af45bef9823efa147d8e
akhiladepu <akhil9394021176@gmail.com>
2022-07-12 17:08:52 +0530

    Entire setup done: files and project structure completed

---
+++ src/Components/index.jsx
@@ -0,0 +1,29 @@
+export { Button } from './Button';
+export { ThemeSettings } from './ThemeSettings';
+export { Sidebar } from './Sidebar';
+export { Navbar } from './Navbar';
+export { Footer } from './Footer';
+export { Cart } from './Cart';
+export { Chat } from './Chat';
+export { Notification } from './Notification';
+export { UserProfile } from './UserProfile';
+export { SparkLine } from './Charts/SparkLine';
+export { LineChart } from './Charts/LineChart';
+export { Stacked } from './Charts/Stacked';
+export { Pie } from './Charts/Pie';
+export { ChartsHeader } from './ChartsHeader';
+export { Header } from './Header';
+
+
+// if we have used rafce for auto completing the export, we need to use this way as in the git source code
+
+// import React from 'react'
+// const Stacked = () => {
+//   return (
+//     <div>Stacked</div>
+//   )
+// }
+
+// export default Stacked
+
+// export { Stacked } from './Charts/Stacked';
---
+++ src/Pages/index.jsx
@@ -0,0 +1,30 @@
+export { Ecommerce } from './Ecommerce';
+export { Kanban } from './Kanban';
+export { Orders } from './Orders';
+export { Employees } from './Employees';
+export { Editor } from './Editor';
+export { Customers } from './Customers';
+export { ColorPicker } from './ColorPicker';
+export { Calendar } from './Calendar';
+export { Area } from './Charts/Area';
+export { Bar } from './Charts/Bar';
+export { ColorMapping } from './Charts/ColorMapping';
+export { Financial } from './Charts/Financial';
+export { Line } from './Charts/Line';
+export { Pie } from './Charts/Pie';
+export { Pyramid } from './Charts/Pyramid';
+export { Stacked } from './Charts/Stacked';
+
+// if we have used rafce for auto completing the export, we need to use this way as in the git source code
+
+// import React from 'react'
+// const Stacked = () => {
+//   return (
+//     <div>Stacked</div>
+//   )
+// }
+
+// export default Stacked
+
+// export { Stacked } from './Charts/Stacked';
+
