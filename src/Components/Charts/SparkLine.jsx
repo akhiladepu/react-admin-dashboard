3dd701a9091904c42b0618bcd4702926850c2667
akhiladepu <akhil9394021176@gmail.com>
2022-07-15 15:35:56 +0530

    Revenue Section in Ecommerce Page

--- src/Components/Charts/SparkLine.jsx
+++ src/Components/Charts/SparkLine.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip, TrackLineSettings } from '@syncfusion/ej2-react-charts';
 
-export const SparkLine = () => {
-  return (
-    <div>SparkLine</div>
-  )
+export const SparkLine = ({ currentColor, id, type, height, width, data, color }) => {
+  return (<SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName='x'
+      yName='yval'
+      type={type}
+      tooltipSettings={{
+        visible: true,
+        format: '${x} : data ${yval}',
+        trackLineSettings: {
+          visible: true
+        }
+      }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>);
 }
--- src/Pages/Orders.jsx
+++ src/Pages/Orders.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { GridComponent, columnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from "@syncfusion/ej2-react-grids";
+import { ordersData, contextMenuItems, ordersGrid } from "../Data/dummy"
+import { Header } from "../Components";
 
 export const Orders = () => {
   return (
-    <div>Orders</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Orders" />
+    </div>
   )
 }
